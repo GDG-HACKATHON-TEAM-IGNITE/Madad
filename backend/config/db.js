1339006e1563b6218a4a3e0f10192d66356ca04e
iampinaki1 <pinaki82499730@gmail.com>
2026-01-10 07:43:34 +0530

    updated connect code check

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,8 @@ dotenv.config();
 const DB_NAME="gdghackathon";
 const connectDB = async () => {
   try {
+    console.log("ENV CHECK:", process.env.MONGODB_URL ? "LOADED" : "NOT LOADED");
+
     const conn = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
