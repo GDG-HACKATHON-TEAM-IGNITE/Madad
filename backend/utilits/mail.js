66892702131cfebe6cbcddea3d2e361a2741613b
iampinaki1 <pinaki82499730@gmail.com>
2026-01-12 19:54:10 +0530

    updated:smtp secure value and port

--- backend/utilits/mail.js
+++ backend/utilits/mail.js
@@ -14,7 +14,7 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false, // TLS on 587
+  secure: true, // TLS on 587
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
